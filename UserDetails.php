0a18eeb964bc6de7fdf3e4ffd887bc31b8d8e29b
VD_Cuddles <apocalaeon@hotmail.com>
2017-06-16 03:54:48 +1200

    Account disabling done

--- UserDetails.php
+++ UserDetails.php
@@ -12,24 +12,37 @@
 		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
 	  // query the users table for name and surname 
-	  $sql = "SELECT Username, Password FROM customers Where Username='".$inputUsername."' AND Password='".$inputPassword."'";
+	  $sql = "SELECT Username, Password, Enabled FROM customers Where Username='".$inputUsername."' AND Password='".$inputPassword."'";
 
 	 //Execute query
 	$rs=$mysqli->query($sql);
 	if (!$rs)
 	  {exit("Error in SQL");}
 
+        //Select the result
+        while ($row = $rs->fetch_assoc())
+        {
+          $enabled=$row["Enabled"];
+        }
+
 	//Count the record number
 	$counter = $rs->num_rows;
       
-	  if ($counter>0)
+	  if ($counter>0 and $enabled == 1)
 	  {
 		  //authentication succeeded
+              echo "Logged in successfully";
 		  return (true);
 	  }
 	  else
 	  {
 		  //authentication failed
+              if($counter<=0){
+                  echo "Account does not exist or password is incorrect.";
+              }
+              else if($enabled!=1){
+                  echo "This account has been disabled.";
+              }
 		  return (false);	
 	  }
    }
